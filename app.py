c256e266162789c366ad05ccc9613d8c528d6239
alekseychuk-ivan <alekseychuk.ivan@gmail.ru>
2023-05-16 23:29:50 +0300

    update

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import torch
 from ultralytics import YOLO
 from pathlib import Path
 from paddleocr import PaddleOCR
 from PIL import Image
 import io
-from fastapi import FastAPI, Request, Form, File, UploadFile
-from fastapi.responses import HTMLResponse, Response
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import Response, StreamingResponse
+import cv2
+import numpy as np
 
 weights = weight = Path('detect/weights/model.pt')
 model = YOLO(model=weights)
@@ -34,101 +36,84 @@ app = FastAPI(
 
 @app.get('/notify/v1/health')
 def get_health():
-  return dict(msg='OK')
+    return dict(msg='OK')
 
 
-@app.post("/object-to-json")
-async def detect_food_return_json_result(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    results = model(input_image)
-    detect_res = results.pandas().xyxy[0].to_json(orient="records")
-    detect_res = json.loads(detect_res)
-    return {"result": detect_res}
+@app.get('/notify/cuda')
+def check_cuda():
+    return dict(device='cuda' if torch.cuda.is_available() else 'cpu')
 
-@app.post("/object-to-img")
-async def detect_food_return_base64_img(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    results = model(input_image)
-    results.render()  # updates results.imgs with boxes and labels
-    for img in results.imgs:
-        bytes_io = io.BytesIO()
-        img_base64 = Image.fromarray(img)
-        img_base64.save(bytes_io, format="jpeg")
-    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+
+@app.post("/detectPIL")
+async def detect(file: UploadFile = File(...)):
+    # since = time.time()
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO(contents)).convert('RGB')
+    pil_image = pil_image.resize((224, 224))
+    filtered_image = io.BytesIO()
+    pil_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+    # print(time.time() - since)
+    # print(type(filtered_image))
+
+    return StreamingResponse(content=filtered_image, media_type="image/jpeg")
+
+
+@app.post("/detectCV")
+async def detect(file: UploadFile = File(...)):
+    # since = time.time()
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    res, im_png = cv2.imencode(".jpeg", img)
+
+    # print(time.time() - since)
+    # print(type(io.BytesIO(im_png.tobytes())))
+    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/jpeg")
+
+@app.post("/detectPILjson")
+async def detect(file: UploadFile = File(...)):
+    # since = time.time()
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO(contents)).convert('RGB')
+    pil_image = pil_image.resize((224, 224))
+    filtered_image = io.BytesIO()
+    pil_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+    # print(time.time() - since)
+    print(type(filtered_image.getvalue()))
+
+    return Response(content=filtered_image.getvalue(), headers=dict([('1', '2')]),  media_type="image/jpeg")
+#
+#
+# @app.post("/detectCV")
+# async def detect(file: UploadFile = File(...)):
+#     # since = time.time()
+#     contents = await file.read()
+#     nparr = np.fromstring(contents, np.uint8)
+#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+#     res, im_png = cv2.imencode(".jpeg", img)
+#
+#     # print(time.time() - since)
+#     print(type(im_png.tobytes()))
+#     return Response(im_png.tobytes(), media_type="image/jpeg")
 
 
 def get_image_from_bytes(binary_image, max_size=1024):
-  input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
-  width, height = input_image.size
-  resize_factor = min(max_size / width, max_size / height)
-  resized_image = input_image.resize((
-    int(input_image.width * resize_factor),
-    int(input_image.height * resize_factor)
-  ))
-  return resized_image
-
-# @app.get("/")
-# async def home(request: Request):
-#   ''' Returns barebones HTML form allowing the user to select a file and model '''
-#
-#   html_content = '''
-# <form method="post" enctype="multipart/form-data">
-#   <div>
-#     <label>Upload Image</label>
-#     <input name="file" type="file" multiple>
-#     <div>
-#       <label>Select YOLO Model</label>
-#       <select name="model_name">
-#         <option>yolov5s</option>
-#         <option>yolov5m</option>
-#         <option>yolov5l</option>
-#         <option>yolov5x</option>
-#       </select>
-#     </div>
-#   </div>
-#   <button type="submit">Submit</button>
-# </form>
-# '''
-#   return HTMLResponse(content=html_content, status_code=200)
-#
-#
-# @app.post("/")
-# async def process_home_form(file: UploadFile = File(...),
-#                             model_name: str = Form(...)):
-#   '''
-#   '''
-#
-#   # model = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True, force_reload=False)
-#
-#   # This is how you decode + process image with PIL
-#   img = Image.open(BytesIO(await file.read()))
-#   results = model(img)
-#
-#   # This is how you decode + process image with OpenCV + numpy
-#   # results = model(cv2.cvtColor(cv2.imdecode(np.fromstring(await file.read(), np.uint8), cv2.IMREAD_COLOR), cv2.COLOR_RGB2BGR))
-#
-#   json_results = results_to_json(results, model)
-#   return json_results
-#
-#
-# def results_to_json(results, model):
-#   ''' Helper function for process_home_form()'''
-#   return [
-#     [
-#       {
-#         "class": int(pred[5]),
-#         "class_name": model.model.names[int(pred[5])],
-#         "bbox": [int(x) for x in pred[:4].tolist()],  # convert bbox results to int from float
-#         "confidence": float(pred[4]),
-#       }
-#       for pred in result
-#     ]
-#     for result in results.xyxy
-#   ]
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize((
+        int(input_image.width * resize_factor),
+        int(input_image.height * resize_factor)
+    ))
+    return resized_image
 
 
 if __name__ == '__main__':
-  import uvicorn
+    import uvicorn
 
-  app_str = 'app:app'
-  uvicorn.run(app_str, host='localhost', port=8000, reload=True, workers=1)
+    app_str = 'app:app'
+    uvicorn.run(app_str, host='localhost', port=8000, reload=True, workers=1)
